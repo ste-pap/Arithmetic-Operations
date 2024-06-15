3b80f9e018834f2a786e148b790700e6421f5a5a
ekdikitis112 <steliosp726@gmail.com>
2024-06-15 19:37:11 +0300

    wrote comments to make code readable

--- Ergasia_2_a/Ergasia_2_a.asm
+++ Ergasia_2_a/Ergasia_2_a.asm
@@ -1,33 +1,33 @@
 title First Program
 include Irvine32.inc
 
-p equ 8d
+p equ 8d              ; Define constant p as 8
 
 .data
-w sdword ?
-x sdword 16d
-y sdword 154d
-z sdword -17d
+w sdword ?            ; Define variable w as a signed doubleword (initialized to 0)
+x sdword 16d          ; Initialize x to 16
+y sdword 154d         ; Initialize y to 154
+z sdword -17d         ; Initialize z to -17
 
 .code
 main PROC
 
-mov eax,x
-add eax,y
+mov eax,x             ; Move value of x (16) into eax
+add eax,y             ; Add y (154) to eax (eax = 16 + 154 = 170)
 
-mov ebx,50d
-add ebx,x
-add ebx,y
-add ebx,z
-sub ebx,p
+mov ebx,50d           ; Move immediate value 50 into ebx
+add ebx,x             ; Add x (16) to ebx (ebx = 50 + 16 = 66)
+add ebx,y             ; Add y (154) to ebx (ebx = 66 + 154 = 220)
+add ebx,z             ; Add z (-17) to ebx (ebx = 220 - 17 = 203)
+sub ebx,p             ; Subtract p (8) from ebx (ebx = 203 - 8 = 195)
 
-sub eax,ebx
-mov w,eax
-mov eax,w ; ???
+sub eax,ebx           ; Subtract ebx (195) from eax (170) and store result in eax (eax = 170 - 195 = -25)
+mov w,eax             ; Move eax (result of the subtraction) into w
+mov eax,w             ; Move w (which now holds eax's original value) back into eax (not affecting anything)
 
-call WriteInt
-call Crlf
+call WriteInt         ; Call Irvine32 procedure to write the integer value in eax
+call Crlf             ; Call Irvine32 procedure to output a newline
 
 exit
 main ENDP
-end main 
+end main
--- Ergasia_2_b/Ergasia_2_b.asm
+++ Ergasia_2_b/Ergasia_2_b.asm
@@ -1,32 +1,34 @@
 title Second Program
 include Irvine32.inc
 
-p equ 8d
+
+p equ 8d    ; Define constant p as 8
 
 .data
-w sdword ?
-x sdword 16d
-y sdword 154d
-z sdword -17d
+w sdword ?   ; Define variable w as a signed doubleword (initialized to 0)
+x sdword 16d  ; Initialize x to 16
+y sdword 154d ; Initialize y to 154
+z sdword -17d ; Initialize z to -17
 
 .code
 main PROC
 
-mov eax,x
-add eax,y
+mov eax,x     ; Move value of x into eax
+add eax,y     ; Add y to eax (eax = x + y)
 
-mov ebx,50d
-add ebx,eax
-add ebx,z
-sub ebx,p
+mov ebx,50d   ; Move immediate value 50 into ebx
+add ebx,eax   ; Add eax to ebx (ebx = 50 + x + y)
+add ebx,z     ; Add z to ebx (ebx = 50 + x + y + z)
+sub ebx,p     ; Subtract p from ebx (ebx = 50 + x + y + z - p)
 
-sub eax,ebx
-mov w,eax
-mov eax,w ; ???
+sub eax,ebx   ; Subtract ebx from eax and store result in eax (eax = (x + y) - (50 + x + y + z - p))
+mov w,eax     ; Move eax (result of the subtraction) into w
+mov eax,w     ; Move w (which now holds eax's original value) back into eax (not affecting anything)
 
-call WriteInt
-call Crlf
+call WriteInt  ; Call Irvine32 procedure to write the integer value in eax
+call Crlf      ; Call Irvine32 procedure to output a newline
 
-exit
+exit           ; Exit the program
+
 main ENDP
-end main 
+end main
